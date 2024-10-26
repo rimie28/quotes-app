90b00190737dda774fc0de3252d1dda446e21d32
Meerim Maratova <maratovameerim@gmail.com>
2024-10-26 18:54:46 +0600

    created Quotes component for showing all quotes and added all components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './Components/Navbar.tsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Sidebar from './Components/Sidebar.tsx';
+import SubmitQuote from './Components/SubmitQuote.tsx';
+import Quotes from './Components/Quotes.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <Navbar/>
+      <div className="container">
+        <div className="row">
+          <div className="col-4 mt-4">
+            <Sidebar/>
+          </div>
+          <div className="col-8 mt-4">
+            <Routes>
+              <Route path="/" element={<Quotes/>}/>
+              <Route path="/add-quote" element={<SubmitQuote/>}/>
+              <Route path="/categories/:categoryId" element={<Quotes/>}/>
+            </Routes>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
---
+++ src/Components/Quotes.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axiosAPI from '../axiosAPI.ts';
+
+interface Quote {
+  id: string;
+  quote: string;
+  text: string;
+  author: string;
+}
+
+const Quotes = () => {
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+  const {categoryId} = useParams();
+
+  useEffect(() => {
+    const getQuotes = async () => {
+      try {
+        const url = categoryId
+          ? `/quotes.json?orderBy="category"&equalTo="${categoryId}"`
+          : `/quotes.json`;
+
+        const quotesResponse = await axiosAPI.get(url)
+        const data = quotesResponse.data;
+
+        const quotes = Object.keys(data).map(key => ({
+          id: key,
+          ...data[key],
+        }));
+        setQuotes(quotes);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getQuotes();
+  }, [categoryId]);
+
+  return (
+    <div>
+      {quotes.map((quote) => (
+        <div key={quote.id} className="border rounded-4 border-2 p-3 mb-4">
+          <p>{quote.text}</p>
+          <p className="mb-0"> - {quote.author} - </p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Quotes;
